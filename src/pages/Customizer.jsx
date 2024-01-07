de8d5f65553605fb27d1a35a8f6959bb947fc07e
Adarsh Jha <adarshjhaxif@gmail.com>
2024-01-07 18:13:19 +0530

    Frontend Complete

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,33 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useSnapshot } from "valtio";
-import { ColorPicker, CustomButton, FilePicker, Tab } from "../components";
-import config from "../config/config.js";
+
+import config from "../config/config";
 import state from "../store";
 import { download } from "../assets";
-import { downloadCanvasToImage, reader } from "../config/helpers.js";
-import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants.js";
-import { fadeAnimation, slideAnimation } from "../config/motion.js";
-import AIPicker from "../components/AIPicker.jsx";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+import {
+  AIPicker,
+  ColorPicker,
+  CustomButton,
+  FilePicker,
+  Tab,
+} from "../components";
 
 const Customizer = () => {
   const snap = useSnapshot(state);
 
   const [file, setFile] = useState("");
+
   const [prompt, setPrompt] = useState("");
   const [generatingImg, setGeneratingImg] = useState(false);
+
   const [activeEditorTab, setActiveEditorTab] = useState("");
   const [activeFilterTab, setActiveFilterTab] = useState({
     logoShirt: true,
-    stylishShirt: true,
+    stylishShirt: false,
   });
-  // show the tab content depending on the active tab
+
+  // show tab content depending on the activeTab
   const generateTabContent = () => {
     switch (activeEditorTab) {
       case "colorpicker":
         return <ColorPicker />;
       case "filepicker":
-        return <FilePicker readFile={readFile} file={file} setFile={setFile} />;
+        return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
         return (
           <AIPicker
@@ -43,13 +52,11 @@ const Customizer = () => {
   };
 
   const handleSubmit = async (type) => {
-    if (!prompt) {
-      return alert("Please enter a prompt");
-    }
+    if (!prompt) return alert("Please enter a prompt");
 
     try {
-      // call the backend to generate the image
       setGeneratingImg(true);
+
       const response = await fetch("http://localhost:8080/api/v1/dalle", {
         method: "POST",
         headers: {
@@ -64,7 +71,7 @@ const Customizer = () => {
 
       handleDecals(type, `data:image/png;base64,${data.photo}`);
     } catch (error) {
-      alert(error.message);
+      alert(error);
     } finally {
       setGeneratingImg(false);
       setActiveEditorTab("");
@@ -73,6 +80,7 @@ const Customizer = () => {
 
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
+
     state[decalType.stateProperty] = result;
 
     if (!activeFilterTab[decalType.filterTab]) {
@@ -84,11 +92,14 @@ const Customizer = () => {
     switch (tabName) {
       case "logoShirt":
         state.isLogoTexture = !activeFilterTab[tabName];
+        break;
       case "stylishShirt":
         state.isFullTexture = !activeFilterTab[tabName];
+        break;
       default:
-        state.isFullTexture = false;
         state.isLogoTexture = true;
+        state.isFullTexture = false;
+        break;
     }
 
     // after setting the state, activeFilterTab is updated
@@ -114,7 +125,7 @@ const Customizer = () => {
         <>
           <motion.div
             key="custom"
-            className=" absolute top-0 left-0 z-10"
+            className="absolute top-0 left-0 z-10"
             {...slideAnimation("left")}
           >
             <div className="flex items-center min-h-screen">
@@ -140,7 +151,7 @@ const Customizer = () => {
               type="filled"
               title="Go Back"
               handleClick={() => (state.intro = true)}
-              customStyle="w-fit px-4 py-2.5 font-bold text-sm"
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
             />
           </motion.div>
 
@@ -157,6 +168,13 @@ const Customizer = () => {
                 handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
+            <button className="download-btn" onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt="download_image"
+                className="w-3/5 h-3/5 object-contain"
+              />
+            </button>
           </motion.div>
         </>
       )}
