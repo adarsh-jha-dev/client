3b7ae5f550a93b29084863ee8ec575f115de84ba
Adarsh Jha <adarshjhaxif@gmail.com>
2024-01-09 10:09:11 +0530

    Few build fixes

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import CustomButton from "./CustomButton";
-import AIPicker from "./AIPicker";
+import AIPicker from "./AIPicker.jsx";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab";
 import ColorPicker from "./ColorPicker";
