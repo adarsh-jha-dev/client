f8b57f6ccdb659ffaf492c7ab9a389320d44b8f1
Adarsh Jha <adarshjhaxif@gmail.com>
2024-01-09 09:57:11 +0530

    Small deployment fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import CustomButton from "./CustomButton";
-import AIPicker from "./AIPicker";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab";
 import ColorPicker from "./ColorPicker";
 
-export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab };
+export { CustomButton, ColorPicker, FilePicker, Tab };
